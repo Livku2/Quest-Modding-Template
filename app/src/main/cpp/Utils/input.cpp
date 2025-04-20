b5b95663b3c1cf4b7ef582d85a8a85e0fc8ba99b
Livku <wellingtonhilbert@outlook.com>
2025-04-20 12:35:12 +0100

    Update input.cpp
    Forgot to add controller

--- app/src/main/cpp/Utils/input.cpp
+++ app/src/main/cpp/Utils/input.cpp
@@ -5,5 +5,5 @@ using namespace BNM;
 bool OVRInput::Get(int Button, int Controller) {
     Class inputClass = Class("", "OVRInput", Image("Oculus.VR.dll"));
     Method<bool> method = inputClass.GetMethod("Get", 2);
-    return method.Call(Button);
+    return method.Call(Button, Controller);
 }
